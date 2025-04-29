dbb2555066c86769eef19e094e905b2acc472afb
manoro <manuel.oro@eduvaud.ch>
2025-04-29 13:19:13 +0200

    feat: lot repository and data samples

---
+++ src/main/java/ch/zucchinit/zauction/Lot/LotRepository.java
@@ -0,0 +1,8 @@
+package ch.zucchinit.zauction.Lot;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface LotRepository extends JpaRepository<Lot, Long>, JpaSpecificationExecutor<Lot> {
+
+}
