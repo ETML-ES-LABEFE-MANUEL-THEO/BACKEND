294122f10aa42169bca838d90458eb246ff76c9e
manoro <manuel.oro@eduvaud.ch>
2025-04-29 13:22:32 +0200

    feat: Lot DTOs for paginated thumbnails and details

---
+++ src/main/java/ch/zucchinit/zauction/Lot/LotDTO.java
@@ -0,0 +1,23 @@
+package ch.zucchinit.zauction.Lot;
+
+import ch.zucchinit.zauction.Category.Category;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class LotDTO {
+    public record LotPaginatedThumbnail(LotThumbnail[] content, Integer page, Integer take, Integer total) {}
+    public record LotThumbnail(Long id, String name, String location, BigDecimal initialPrice, BigDecimal lastPrice, String thumbnail) {};
+    public record LotDetails(Long id,
+                             String name,
+                             String description,
+                             String location,
+                             BigDecimal initialPrice,
+                             BigDecimal lastPrice,
+                             String[] medias,
+                             LocalDate openDate,
+                             LocalDate awardDate,
+                             LocalDate closeDate,
+                             @JsonIgnoreProperties("children") Category category) {}
+}
