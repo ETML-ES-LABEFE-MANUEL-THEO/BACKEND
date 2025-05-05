a84857e27afee8d090f4929cdb1247d654c63357
manoro <manuel.oro@eduvaud.ch>
2025-05-05 09:12:23 +0200

    feat: exceptions handlers (404, 400 with errors, 500)

---
+++ src/main/java/ch/zucchinit/zauction/Exceptions/ExceptionsAdvice.java
@@ -0,0 +1,45 @@
+package ch.zucchinit.zauction.Exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.*;
+
+@ControllerAdvice
+public class ExceptionsAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(ResourceNotFound.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    void ResourceNotFoundHandler(ResourceNotFound ex) {}
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    List<ExceptionsDTO.ValidationError> ValidationErrorHandler(MethodArgumentNotValidException ex) {
+        Map<String, List<String>> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String field = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+
+            if (errors.containsKey(errorMessage)) {
+                errors.get(errorMessage).add(field);
+            } else {
+                errors.put(errorMessage, List.of(field));
+            }
+        });
+
+        return errors.entrySet().stream().map(err -> new ExceptionsDTO.ValidationError(err.getValue(), err.getKey())).toList();
+    }
+
+    @ResponseBody
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    ExceptionsDTO.GenericError UnknownErrorHandler(Exception ex) {
+        return new ExceptionsDTO.GenericError(ex.getMessage());
+    }
+}
